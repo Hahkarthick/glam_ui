e316e0e14b163015ea73a8418b8584356cff0cde
Harish <abhirami@sterliteusa.com>
2018-10-17 19:37:32 +0530

    fixes

--- src/app/layout/blogListing/blogListing.component.ts
+++ src/app/layout/blogListing/blogListing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ListingService } from '../../service/listing.service';
 @Component({
     selector: 'app-bloglisting',
@@ -13,9 +13,16 @@ export class BlogListingComponent implements OnInit, AfterViewInit {
 
     protected blogs: Array<any> = [];
 
-    constructor(private renderer: Renderer2, private listing: ListingService, private sanitizer: DomSanitizer, private router: Router) {
+    constructor(
+        private renderer: Renderer2,
+        private route: ActivatedRoute,
+        private listing: ListingService,
+        private sanitizer: DomSanitizer,
+        private router: Router) {
 
-/*         this.blogs.push(
+        const id = +this.route.snapshot.params['id'];
+        /*
+            this.blogs.push(
             {
                 blog_source: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/CvP92martxs'),
                 blog_contents : 'He was a complete gentleman as he held the sheet up so I could turn over onto my back.' +
@@ -28,7 +35,8 @@ export class BlogListingComponent implements OnInit, AfterViewInit {
                                 ' chest to my sides. We were alone in the room at this point but if someone had walked in it would have  ' +
                                 ' looked completely innocent. Everything he did was oh-so subtle.'
             }
-        ); */
+            );
+        */
     }
 
     ngAfterViewInit() {
